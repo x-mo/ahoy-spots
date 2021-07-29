61573fc0709819ba256c92819ef60163854aee93
x-mo <X_MohamedAhmed@yahoo.com>
2021-07-29 20:23:50 +0400

    first commit

---
+++ app/src/main/java/com/xmo/spots/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xmo.spots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
