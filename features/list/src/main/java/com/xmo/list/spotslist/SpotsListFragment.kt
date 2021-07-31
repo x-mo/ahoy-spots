b5c65260a681c6233761358c09222dacccb6d014
x-mo <X_MohamedAhmed@yahoo.com>
2021-07-31 15:11:31 +0400

    SpotsList: Permission Request, Obtain Fine Location, and fetch from API

--- features/list/src/main/java/com/xmo/list/spotslist/SpotsListFragment.kt
+++ features/list/src/main/java/com/xmo/list/spotslist/SpotsListFragment.kt
@@ -1,10 +1,21 @@
 package com.xmo.list.spotslist
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.permissionx.guolindev.PermissionX
 import com.xmo.core.base.BaseFragment
 import com.xmo.core.extensions.observe
 import com.xmo.list.R
@@ -13,6 +24,8 @@ import com.xmo.list.spotslist.di.DaggerSpotsListComponent
 import com.xmo.list.spotslist.di.SpotsListModule
 import com.xmo.spots.BaseApp
 import javax.inject.Inject
+
+private const val LOCATION_PERMISSION_REQUEST_CODE: Int = 3193
 
 class SpotsListFragment :
     BaseFragment<FragmentSpotListBinding, SpotsListViewModel>(
@@ -27,7 +40,8 @@ class SpotsListFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observe(viewModel.state, ::onViewStateChange)
-        viewModel.loadSpotsList(25.204849,55.270782)
+
+        requestLocationPermissionAndObtainLatLng()
 
         observe(viewModel.data) { Log.d("xox", it.toString()) }
     }
@@ -57,4 +71,61 @@ class SpotsListFragment :
             else -> progressDialog.visibility = View.GONE
         }
     }
+
+//--------!!!Refactor This Later!!!---------------------------------------------------------------------------------------------
+    private fun requestLocationPermissionAndObtainLatLng() {
+        PermissionX.init(this)
+            .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
+            .onExplainRequestReason { scope, deniedList ->
+                scope.showRequestReasonDialog(
+                    deniedList, getString(R.string.spots_only_available_if), getString(
+                        R.string.ok
+                    ), getString(R.string.cancel)
+                )
+            }
+            .request { allGranted, _, _ ->
+                if (allGranted) {
+                    setUpLocationListener()
+                } else {
+                    Toast.makeText(
+                        activity,
+                        getString(R.string.location_denied_app),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setUpLocationListener() {
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        // for getting the current location update after every 2 seconds with high accuracy
+        val locationRequest = LocationRequest.create().apply {
+            interval = 2000
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            maxWaitTime = 2000
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for (location in locationResult.locations) {
+
+                        viewModel.loadSpotsList(location.latitude, location.longitude)
+                        Log.d("lat---", location.latitude.toString())
+                        Log.d("lng---", location.longitude.toString())
+                        //stop location updates
+                        fusedLocationProviderClient.removeLocationUpdates(this);
+
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
+    }
+
 }
